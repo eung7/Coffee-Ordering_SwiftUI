4eb01c3a124967da5a0a970335f498df491890c0
eung7 <wannabeeung@gmail.com>
2022-07-03 14:59:14 +0900

    First Commit

---
+++ Coffee Ordering/Coffee_OrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_OrderingApp.swift
+//  Coffee Ordering
+//
+//  Created by 김응철 on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_OrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
