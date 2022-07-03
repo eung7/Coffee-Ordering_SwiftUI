9c3b1ffb986d79e34c918dabd52397358a180a82
eung7 <wannabeeung@gmail.com>
2022-07-03 17:58:35 +0900

    Creating Webservice

---
+++ Coffee Ordering/Services/WebService.swift
@@ -0,0 +1,21 @@
+//
+//  WebService.swift
+//  Coffee Ordering
+//
+//  Created by 김응철 on 2022/07/03.
+//
+
+import Foundation
+
+class WebService {
+    func getAllOrders(completion: @escaping ([Order]?) -> Void) {
+        guard let url = URL(string: "https://island-bramble.glitch.me/orders") else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let orders = try? JSONDecoder().decode([Order].self, from: data)
+            DispatchQueue.main.async {
+                completion(orders)
+            }
+        }.resume()
+    }
+}
