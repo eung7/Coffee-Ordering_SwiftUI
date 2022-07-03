db64fa60b8d29e4a17d91c2c124b573c10a21864
eung7 <wannabeeung@gmail.com>
2022-07-03 20:42:55 +0900

    Implementing Create Coffee Order Webservice Function

---
+++ Coffee Ordering/Models/CreateOrderResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CreateOrderResponse.swift
+//  Coffee Ordering
+//
+//  Created by 김응철 on 2022/07/03.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+    let success: Bool
+}
--- Coffee Ordering/Services/WebService.swift
+++ Coffee Ordering/Services/WebService.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 class WebService {
+    func createCoffeeOrder(order: Order, completion: @escaping (CreateOrderResponse?) -> Void) {
+        guard let url = URL(string: "https://island-bramble.glitch.me/orders") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(order)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let createOrderResponse = try? JSONDecoder().decode(CreateOrderResponse.self, from: data)
+            DispatchQueue.main.async {
+                completion(createOrderResponse)
+            }
+        }.resume()
+    }
+    
     func getAllOrders(completion: @escaping ([Order]?) -> Void) {
         guard let url = URL(string: "https://island-bramble.glitch.me/orders") else { return }
         URLSession.shared.dataTask(with: url) { data, response, error in
@@ -19,4 +35,3 @@ class WebService {
         }.resume()
     }
 }
-
